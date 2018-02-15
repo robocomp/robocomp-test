c0a046b199c421ba8a08c7a92056707f4235494d
Pablo Bustos <pbustos@unex.es>
2018-02-15 14:14:10 +0100

    asdfa

--- inner/inner.cpp
+++ inner/inner.cpp
@@ -1,14 +1,13 @@
-
-
-using namespace std;
+#include <iostream>
+#include <thread>
+#include <unordered_map>
+#include <string>
 
 class ROOT
 {
-	using namespace std;
-
 	ROOT()
 	{
-		cout << "Soy ROOT" << endl;
+		std::cout << "Soy ROOT" << std::endl;
 	}
 };
 
@@ -16,7 +15,7 @@ class TRANSFORM
 {
 	TRANSFORM()
 	{
-		cout << "Soy TRANSFORM" << endl;
+		std::cout << "Soy TRANSFORM" << std::endl;
 	}
 };
 
@@ -24,32 +23,41 @@ class CAMERA
 {
 	CAMERA()
 	{
-		cout << "Soy CAMERA" << endl;
+		std::cout << "Soy CAMERA" << std::endl;
 	}
 };
 
-
-////////////////////////////////
-/// Factory constructor
-///////////////////////////////
-
-template<typename T, typename... Ts>
-std::shared_ptr<T> newNode(Ts&&... params)
-	{
-		auto t = std::make_tuple(params...);
-		QString id = QString(std::get<0>(t));
-		if(hash->contains(id))
-			throw InnerModelException("InnerModel::newNode Error: Cannot insert new node with already existing name" + id.toStdString());
-		
-		std::shared_ptr<T> node(new T(std::forward<Ts>(params)...)); 
-		hash->insert(id, std::static_pointer_cast<InnerModelNode>(node));
-		return node;
-	}
-
+class Inner
+{
+	
+	public:
+		using ROOTPtr = std::shared_ptr<ROOT>; 
+		Inner(){};
+		////////////////////////////////
+		/// Factory constructor
+		///////////////////////////////
+		template<typename T, typename... Ts>
+		std::shared_ptr<T> newNode(Ts&&... params)
+		{
+			auto t = std::make_tuple(params...);
+			std::string id = (std::get<0>(t));
+			std::shared_ptr<T> node(new T(std::forward<Ts>(params)...)); 
+			hash.try_emplace(id, std::static_pointer_cast<ROOT>(node));
+			return node;
+		}
+		void setRoot(RootPtr r)		{ root = r;};
+	private:
+		std::unordered_map<std::string, ROOT> hash;
+		ROOTPtr root;
+};
 
 int main()
 {
 
-
+	Inner inner = std::shared_ptr<Inner>();
+	inner->setRoot( inner->newNode<TRANSFORM>() );
+	
+	
+	
 
 }
