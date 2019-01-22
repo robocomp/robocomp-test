29da79db785d06c896953526fc7fa4702ec20ae7
pbustos <pbustos@unex.es>
2019-01-22 22:12:13 +0100

    almost there

--- elasticpath/src/specificworker.cpp
+++ elasticpath/src/specificworker.cpp
@@ -58,7 +58,7 @@ void SpecificWorker::initialize(int period)
 
 	std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> dis(-30, 30);
+    std::uniform_int_distribution<> dis(-20, 20);
 	for(auto i: iter::range(-180, 180, 20))
 	{
 		auto ellipse = scene.addEllipse(QRectF(0,0, 10, 10), QPen(Qt::blue), QBrush(Qt::blue));
@@ -70,8 +70,11 @@ void SpecificWorker::initialize(int period)
 	}
 	first = points.front();
 	first->setPos(first->x(),0);
+	first->setRect(0,0, 20, 20);
+	first->setBrush(QColor("MediumGreen"));
 	last = points.back();
 	last->setPos(last->x(),0);
+	last->setBrush(QColor("LightBlue"));
 
 	// Obstacle
 	box = scene.addRect(QRectF(0,0, 50,50), QPen(Qt::magenta), QBrush(Qt::magenta));
@@ -85,16 +88,17 @@ void SpecificWorker::initialize(int period)
 		laserData.emplace_back(LData{0.f, (float)i});
 		poly << QPointF(1.f*cos(i), 1.f*sin(i));
 	};
-	polygon = scene.addPolygon(poly);
+	QBrush brush; brush.setColor(QColor("LightPink")); brush.setStyle(Qt::Dense6Pattern);
+	polygon = scene.addPolygon(poly, QPen(QColor("LighPink")), brush);
 	
-	timer.start(50);
+	timer.start(30);
 	//timer.setSingleShot(true);
 }
 
 void SpecificWorker::compute()
 {
 	computeForces();
-	computeLaser(points[0], box);
+	computeLaser(first, box);
 
 }
 
@@ -102,8 +106,8 @@ void SpecificWorker::computeForces()
 {
 	if(points.size() < 3) return;
 
-	std::vector<QVector2D> iforces(points.size(), QVector2D(0.f,0.f));
-	std::vector<QVector2D> speed(points.size(), QVector2D(0.f,0.f));
+	// baselines needed to remove tangential component
+	std::vector<QVector2D> base_lines(points.size(), QVector2D(0.f,0.f));
 	int k=1;
 	for(auto group : iter::sliding_window(points, 3))
 	{
@@ -113,37 +117,95 @@ void SpecificWorker::computeForces()
 		auto p2 = QVector2D(group[1]->pos());
 		auto p3 = QVector2D(group[2]->pos());
 
+		base_lines[k++] = (p1 - p3).normalized();
+	}  
+
+	// internal curvature forces
+	std::vector<QVector2D> iforces(points.size(), QVector2D(0.f,0.f));
+	k=1;
+	for(auto group : iter::sliding_window(points, 3))
+	{
+		if(group.size()<3) break;
+
+		auto p1 = QVector2D(group[0]->pos());
+		auto p2 = QVector2D(group[1]->pos());
+		auto p3 = QVector2D(group[2]->pos());
+
 		//internal force on p2
-		auto iforce = 0.5*((p1-p2)/(p1-p2).length() + (p3-p2)/(p3-p2).length());
+		auto iforce = ((p1-p2)/(p1-p2).length() + (p3-p2)/(p3-p2).length());
 		iforces[k++] = iforce;
 	}  
-	
-	for(auto &&[point, force] : iter::zip(points, iforces))
+
+	// internal elongation forces
+	std::vector<QVector2D> lforces(points.size(), QVector2D(0.f,0.f));
+	const float force_cero = 10;
+	k=0;
+	for(auto group : iter::sliding_window(points, 2))
 	{
-		auto pos = point->pos();
-		auto delta = force.toPointF();
-		//auto l = lf->line();
-		//lf->setLine(pos.x()+5, pos.y(), pos.x()+5, (pos+delta).y());
-		point->setPos( pos.x(), (pos + delta).y()); 
-		// qDebug() << force;
+		if(group.size()<2) break;
+		auto p1 = QVector2D(group[0]->pos());
+		auto p2 = QVector2D(group[1]->pos());
+
+		// f = -kx  Hook's law
+		auto force_mod = (p1-p2).length();
+		lforces[k] = (force_mod - force_cero) * ((p2-p1)/force_mod);
+		lforces[k+1] = (force_mod - force_cero) * ((p1-p2)/force_mod);
+		k++;
+	}  
+	
+	// external forces
+	// we need the minimun distance from each point to the obstacle(s)
+	// we compute the closest laser ray tip to each point
+	std::vector<QVector2D> eforces;
+	for( auto &&p : points)
+	{
+		std::vector<std::tuple<float, QVector2D>> distances;
+		std::transform(std::begin(laserData), std::end(laserData), std::back_inserter(distances), [p, this](auto &l)
+			{ 
+				QVector2D tip(first->mapToScene(QPointF(l.dist*cos(l.angle), l.dist*sin(l.angle))));
+				return std::make_tuple((QVector2D(p->pos()) - tip).length(), QVector2D(p->pos()) - tip);	
+			});
+		auto min = std::min_element(std::begin(distances), std::end(distances), [](auto &a, auto &b){return std::get<0>(a) < std::get<0>(b);});
+		//qDebug() << std::get<0>(*min) << std::get<1>(*min);
+		eforces.emplace_back(std::get<1>(*min));
 	}
-	first->setPos(points.front()->x(),0);
-	last->setPos(points.back()->x(),0);
-	// for(auto i=0; i< points.size(); i++)
-	// {
-	// 	speed[i] = QVector2D(points[i]->pos())- posant[i];
-	// }
+
+	//Apply forces
+	const float KE = 0.09;
+	const float KI = 3;	
+	const float KL = 0.06;	
+	for(auto &&[point, iforce, eforce, lforce, base_line] : iter::zip(points, iforces, eforces, lforces, base_lines))
+	{
+		const auto &idelta = KI * iforce.toPointF();
+		const auto &ldelta = KL * lforce.toPointF();
+
+		QPointF edelta{0,0}; 
+		if( eforce.length() < 100)
+			edelta = KE * eforce.toPointF();
+		
+		//qDebug() << ldelta;
+		//const auto force =  edelta + idelta + ldelta;
+		const auto force =  ldelta;
+
+		// Removing tangential component
+		const auto corrected_force = force - (base_line * QVector2D::dotProduct(QVector2D(force), base_line)).toPointF();
+
+		point->setPos( point->pos() + force ); 
+	}
+	//qDebug() << "-------------------";
+	// reposition endpoints
+	first->setPos(-180,0);
+	last->setPos(180,0);
 }
 
+////// Render synthetic laser
 void SpecificWorker::computeLaser(QGraphicsEllipseItem* ellipse, QGraphicsRectItem *box)
 {
-	int i=0;
-	const float MAX_LASER = 200;
+	const float MAX_LASER = 400;
 	for( auto &l : laserData )
 	{
 		l.dist = MAX_LASER;
 		QLineF line(ellipse->pos(), ellipse->mapToScene(QPointF(MAX_LASER*cos(l.angle), MAX_LASER*sin(l.angle))));
-		//qDebug() << line;
 		for( auto t :  iter::range(0.f,1.f,0.01f) )
 		{
 			auto r = line.pointAt(t);
@@ -156,8 +218,9 @@ void SpecificWorker::computeLaser(QGraphicsEllipseItem* ellipse, QGraphicsRectIt
 	}
 	scene.removeItem(polygon);
 	QPolygonF poly;
+	QBrush brush; brush.setColor(QColor("LightPink")); brush.setStyle(Qt::Dense6Pattern);
 	for(auto &&l : laserData)
 		poly << QPointF(ellipse->pos().x()+l.dist*cos(l.angle),ellipse->pos().y()+l.dist*sin(l.angle));
-	polygon = scene.addPolygon(poly);
+	polygon = scene.addPolygon(poly, QPen(QColor("LightPink")), brush);
 }
 
