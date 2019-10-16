41e53ef30b796df2c34737121c1993ed8391f8d6
pbustos <pbustos@unex.es>
2019-10-16 18:56:07 +0200

    working

--- elasticpath/src/specificworker.cpp
+++ elasticpath/src/specificworker.cpp
@@ -64,8 +64,12 @@ bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList _params)
 void SpecificWorker::initialize(int period)
 {
 	std::cout << "Initialize worker" << std::endl;
+ 	
+	 //Load World
+    initializeWorld();
+	
 	resize(QDesktopWidget().availableGeometry(this).size() * 0.6);
-	scene.setSceneRect(LEFT, BOTTOM, WIDTH, HEIGHT);
+	scene.setSceneRect(dimensions.HMIN, dimensions.VMIN, dimensions.WIDTH, dimensions.HEIGHT);
 	view.scale( 1, -1 );
 	view.setScene(&scene);
 	view.setParent(this);
@@ -82,10 +86,6 @@ void SpecificWorker::initialize(int period)
 	settings.endGroup();
 */
 
-    //Load World
-    initializeWorld();
-
-	
 	// Robot 
 	QPolygonF poly2;
 	float size = ROBOT_LENGTH/2.f;
@@ -138,12 +138,12 @@ void SpecificWorker::initialize(int period)
 		laserData.emplace_back(LData{0.f, (float)i});
 	
 	//Grid
-	grid.initialize( TDim{ TILE_SIZE, LEFT, BOTTOM, WIDTH, HEIGHT}, TCell{0, true, false, nullptr, 1.f} );
+	grid.initialize( dimensions, TCell{0, true, false, nullptr, 1.f} );
+	
 	// check is cell key.x, key.z is free by checking is there are boxes in it
 	std::uint32_t id_cont=0;
 	for(auto &&[k, cell] : grid)
 	{
-		//robot->setPos(k.x, k.z);
 		cell.g_item = scene.addEllipse(QRectF(-200, -200, 200, 200));
 		cell.g_item->setPos(k.x, k.z);
 		cell.g_item->setZValue(-1);		
@@ -157,23 +157,15 @@ void SpecificWorker::initialize(int period)
 		 		cell.g_item->setPen(QPen(QColor("WhiteSmoke")));
 		 		cell.g_item->setBrush(QBrush(QColor("WhiteSmoke")));
 		}
-		// for(auto &&p: robot->polygon())
-		// 	if(std::any_of(std::begin(boxes), std::end(boxes),[p, this](auto &box){ return box->contains(box->mapFromItem(robot, p));}))
-		// 	{
-		// 		cell.free = false;		
-		// 		cell.g_item->setPen(QPen(QColor("WhiteSmoke")));
-		// 		cell.g_item->setBrush(QBrush(QColor("WhiteSmoke")));
-		// 		break;
-		// 	}
 	}
 	// Set high cost to cell touching obstacles
 	for(auto &cell : grid)
 	{
 		auto list_n = grid.neighboors(cell.first); //Key
 		if( std::any_of(std::begin(list_n), std::end(list_n), [](const auto &n){ return n.second.free == false;}))
-			cell.second.cost = 5.f;
+			cell.second.cost = 5.f;			
 	}
-	//robot->setPos(robot_back);
+	
 	qDebug() << "Grid initialize ok";
 
 	// compute
@@ -181,7 +173,7 @@ void SpecificWorker::initialize(int period)
 	
 	// clean path
 	connect(&cleanTimer, &QTimer::timeout, this, &SpecificWorker::cleanPath);
-	//cleanTimer.start(50);
+	cleanTimer.start(50);
 	
 	// controller
     connect(&controllerTimer, &QTimer::timeout, this, &SpecificWorker::controller);
@@ -191,7 +183,7 @@ void SpecificWorker::initialize(int period)
 	//	connect(humanA, &Human::personChangedSignal, this, &SpecificWorker::personChangedSlot);
 	//	connect(humanB, &Human::personChangedSignal, this, &SpecificWorker::personChangedSlot);
 
-	showMaximized();
+	//showMaximized();
 
 }
 
@@ -210,6 +202,10 @@ void SpecificWorker::initializeWorld()
     QJsonDocument doc = QJsonDocument::fromJson(val.toUtf8());
     QJsonObject jObject = doc.object();
     QVariantMap mainMap = jObject.toVariantMap();
+	//load dimensions
+	QVariantMap dim = mainMap[QString("dimensions")].toMap();
+	dimensions = TDim{dim["TILESIZE"].toInt(), dim["LEFT"].toFloat(), dim["BOTTOM"].toFloat(), dim["WIDTH"].toFloat(), dim["HEIGHT"].toFloat()};
+   
     //load tables
     QVariantMap tables = mainMap[QString("tables")].toMap();
     for (auto &t: tables)
@@ -259,8 +255,8 @@ void SpecificWorker::compute()
 	computeLaser(laser_pose, boxes);  // goes
 	computeVisibility(points, laser_polygon);
 	//computeForces(points, laserData);
-	cleanPath();
-	controller();
+	//cleanPath();
+	//controller();
 	updateRobot();				// goes
 	reloj.restart();
 }
@@ -275,6 +271,43 @@ void SpecificWorker::cleanPath()
 /////////////////////////////////////////////////////////////////////7
 /////////
 ////////////////////////////////////////////////////////////////////
+// void SpecificWorker::createPathFromGraph(const std::list<QVec> &path)
+// {
+// 	if(path.empty()) return;
+// 	for(auto &&p: points)
+// 		scene.removeItem(p);
+// 	points.clear();
+// 	for(auto &&p: path)
+// 	{
+// 		auto ellipse = scene.addEllipse(QRectF(-BALL_MIN,-BALL_MIN, BALL_SIZE, BALL_SIZE), QPen(QColor("LightGreen")), QBrush(QColor("LightGreen")));
+// 		ellipse->setFlag(QGraphicsItem::ItemIsMovable);
+// 		ellipse->setPos(p.x(), p.z());
+// 		points.push_back(ellipse);
+// 	}
+// 	first = points.front();
+// 	first->setPos(robot_nose->mapToScene(QPointF(0,0))); 
+// 	first->setBrush(QColor("MediumGreen"));
+// 	////////////////////////// add real target
+// 	last = points.back();
+// 	last->setPos(target->pos());
+// 	target->setZValue(1);
+
+// 	//remove intial points in the path that are too close to the robot excluding robot and target
+// 	std::vector<QGraphicsEllipseItem*> points_to_remove;
+// 	for( auto &&p: iter::slice(points,1,(int)points.size()-1, 1))
+// 	{
+// 		if(robot->polygon().containsPoint(robot->mapFromScene(p->pos()), Qt::OddEvenFill))
+// 		{
+// 			std::vector<QGraphicsEllipseItem*> points_to_remove;
+// 			points_to_remove.push_back(p);
+// 		}
+// 	}
+// 	for(auto p: points_to_remove)
+// 	{
+// 		scene.removeItem(p);
+// 		points.erase(std::remove_if(points.begin(), points.end(), [p](auto &r){ return p==r;}), points.end());
+// 	}
+// }
 
 void SpecificWorker::createPathFromGraph(const std::list<QVec> &path)
 {
@@ -285,7 +318,8 @@ void SpecificWorker::createPathFromGraph(const std::list<QVec> &path)
 	qDebug() << __FUNCTION__ << "hola";
 	for(auto &&p: path)
 	{
-		if((p-QVec::vec2(robot->pos().x(), robot->pos().y())).norm2() > ROBOT_LENGTH * 2)
+		//if((p-QVec::vec2(robot->pos().x(), robot->pos().y())).norm2() > ROBOT_LENGTH * 2)
+		if(robot->polygon().containsPoint(robot->mapFromScene(p.toQPointF()), Qt::OddEvenFill)==false)
 		{ 
 			auto ellipse = scene.addEllipse(QRectF(-BALL_MIN,-BALL_MIN, BALL_SIZE, BALL_SIZE), QPen(QColor("LightGreen")), QBrush(QColor("LightGreen")));
 			ellipse->setFlag(QGraphicsItem::ItemIsMovable);
@@ -303,20 +337,23 @@ void SpecificWorker::createPathFromGraph(const std::list<QVec> &path)
 	target->setZValue(1);
 
 	//remove intial points in the path that are too close to the robot excluding robot and target
-	std::vector<QGraphicsEllipseItem*> points_to_remove;
-	for( auto &&p: iter::slice(points,1,(int)points.size()-1, 1))
-	{
-		if(robot->polygon().containsPoint(robot->mapFromScene(p->pos()), Qt::OddEvenFill))
-		{
-			std::vector<QGraphicsEllipseItem*> points_to_remove;
-			points_to_remove.push_back(p);
-		}
-	}
-	for(auto p: points_to_remove)
-	{
-		scene.removeItem(p);
-		points.erase(std::remove_if(points.begin(), points.end(), [p](auto &r){ return p==r;}), points.end());
-	}
+	// std::vector<QGraphicsEllipseItem*> points_to_remove;
+	// for( auto &&p: iter::slice(points,1,(int)points.size()-1, 1))
+	// {
+	// 	if(robot->polygon().containsPoint(robot->mapFromScene(p->pos()), Qt::OddEvenFill))
+	// 	{
+	// 		std::vector<QGraphicsEllipseItem*> points_to_remove;
+	// 		points_to_remove.push_back(p);
+	// 	}
+	// }
+	// for(auto p: points_to_remove)
+	// {
+	// 	scene.removeItem(p);
+	// 	points.erase(std::remove_if(points.begin(), points.end(), [p](auto &r){ return p==r;}), points.end());
+	// }
+	for(auto &&p: points)
+		std::cout << "(" << p->x() << " " << p->y() << ")" << std::endl;
+	std::cout << std::endl;
 }
 
 void SpecificWorker::computeVisibility(const std::vector<QGraphicsEllipseItem*> &path, const QGraphicsPolygonItem *laser)
@@ -412,10 +449,10 @@ void SpecificWorker::computeForces(const std::vector<QGraphicsEllipseItem*> &pat
 			force = std::get<QVector2D>(*min);
 			float mag = force.length();
 			// compute linear inverse law  y = -1/4 x + 200
-			if(mag > 1000) mag = 1000;
-			mag  = -(200.f/1000)*mag + 200.f;
+			if(mag > 800) mag = 800;
+			mag  = -(200.f/800)*mag + 200.f;
 			f_force = mag * force.normalized();	
-			if(mag < 1000)	
+			if(mag < 800)	
 				lforces.push_back(scene.addLine(QLineF( p->pos(), 1.1*(p->pos()+f_force.toPointF())), 
 							QPen(QBrush(QColor("DarkGreen")),15)));
 			eforces.push_back(f_force);
@@ -424,7 +461,7 @@ void SpecificWorker::computeForces(const std::vector<QGraphicsEllipseItem*> &pat
 
 	//Apply forces to current position
 	const float KE = 0.8;
-	const float KI = 1.4;	
+	const float KI = 1.5;	
 	//const float KL = 0.06;	
 	for(auto &&[point, iforce, eforce, base_line] : iter::zip(lpath, iforces, eforces, base_lines))
 	{
@@ -448,7 +485,7 @@ void SpecificWorker::computeForces(const std::vector<QGraphicsEllipseItem*> &pat
 	// reposition endpoints to robot's nose and target
 	first->setPos(robot_nose->mapToScene(QPointF(0,0)));
 	last->setPos(target->pos());
-	second = points[1];
+	
 }
 
 ////// Add points to the path if needed
@@ -460,6 +497,7 @@ void SpecificWorker::addPoints()
 	{
 		auto &p1 = group[0];
 		auto &p2 = group[1];
+	
 		float dist = QVector2D(p1->pos()-p2->pos()).length();
 		if (dist > ROAD_STEP_SEPARATION)  
 		{
@@ -487,6 +525,7 @@ void SpecificWorker::cleanPoints()
 	{
 		const auto &p1 = group[0];
 		const auto &p2 = group[1];
+	
 		if(p2 == last)
 			break;
 		float dist = QVector2D(p1->pos()-p2->pos()).length();
@@ -651,8 +690,8 @@ void SpecificWorker::mousePressEvent(QMouseEvent *event)
 		std::list<QVec> path = grid.computePath(Grid<TCell>::Key(robot_nose->mapToScene(QPointF(0,0))), Grid<TCell>::Key(p));
 		if(path.size() > 0) 
 		{
-			createPathFromGraph(path);
 			target->setPos(p);
+			createPathFromGraph(path);
             active = true;
 		}
 		// for(auto &&p: path)
